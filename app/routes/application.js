8eba0465def2ff139c99522958977cb712f82859
Andrew Kirwin <andrew.kirwin@zestia.com>
2020-02-13 23:12:14 +0000

    emberA

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,12 +1,12 @@
 import Route from '@ember/routing/route';
 import { inject } from '@ember/service';
+import { A as emberA } from '@ember/array';
 
 export default class ApplicationRoute extends Route {
   @inject store;
 
   model() {
-    // Uncomment this to witness ED's magic tracking
-    // return { name: 'List 1', items: Ember.A([]) };
+    // return { name: 'List 1', items: ember() };
 
     return this.store.createRecord('list', { name: 'List 1' });
   }
